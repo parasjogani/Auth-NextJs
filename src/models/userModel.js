a4a5109f34c1d423140418648c02032bd27f2c3c
parasjogani <paras.jogani@gmail.com>
2023-07-03 21:14:27 +0530

    userSchema add

---
+++ src/models/userModel.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: [true, "Please provide a username"],
+        unique: true,
+    },
+
+    email: {
+        typr: String,
+        required: [true, "Please provide a email"],
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: [true, "Please provide password"],
+    },
+    isVerified: {
+        type: Boolean,
+        default: false,
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false,
+    },
+    forgotPasswordToken: String,
+    forgotPasswordTokenExpiry: Date,
+    verifyToken: String,
+    verifyTokenExpiry: Date,
+})
+
+const User = mongoose.models.users || mongoose.model("User", userSchema)
+
+export default User
